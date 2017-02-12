271c1f51aa952ccbfa98178bee31df3bb6ce770d
Dayne Wright <daynewr@gmail.com>
2017-02-12 00:54:58 -0600

    added formating

--- Data/Queries/Avg_Fuel_Location_Month_Number.sql
+++ Data/Queries/Avg_Fuel_Location_Month_Number.sql
@@ -2,12 +2,12 @@
 # Leaves month as number.
 
 
-SELECT 
-	STRFTIME('%m', S.DateCreated) AS "Month",
+SELECT STRFTIME('%m', S.DateCreated) AS "Month",
 	L.Name AS "Location Name",
 	(SELECT AVG(FuelAmountUsed)
 	 FROM Stop AS S
-	 WHERE STRFTIME('%m', S.DateCreated) AND S.LocationId == L.LocationId) AS "Fuel Use Avg"
+	 WHERE STRFTIME('%m', S.DateCreated) 
+	 AND S.LocationId == L.LocationId) AS "Fuel Use Avg"
 FROM Location AS L, Stop AS S
 WHERE L.LocationId == S.LocationId
 GROUP BY L.Name;
